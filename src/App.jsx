4c2138bc746ee1b6701817f7499cc8b8666cd9fc
Felipe <139351148+felipopm@users.noreply.github.com>
2023-10-26 13:51:23 -0300

    Foi criada uma tabela dinâmica com as informações dos jogos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Card from './components/Card'
+import GameTable from './components/GameTable'
 
 function App() {
 
@@ -8,6 +9,11 @@ function App() {
       <h1>Copa do Mundo Feminina 2023</h1>
       <section className='cards'>
         <Card />
+      </section>
+
+      <h2>Tabela de jogos</h2>
+      <section className='game_table'>
+        <GameTable />
       </section>
     </>
   )
