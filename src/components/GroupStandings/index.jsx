02226bddd40ad38f8bcbbad63dba3a1b046a0e1e
Felipe <139351148+felipopm@users.noreply.github.com>
2023-10-26 18:05:01 -0300

    Foi criado uma caixa de seleção para chamada da tabela de classificação dos respectivos grupos

--- src/components/GroupStandings/index.jsx
+++ src/components/GroupStandings/index.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 function GroupStandings() {
     
     const [ grupos, setGrupos ] = useState([])
+    const [ letraSelecionada, setLetraSelecionada ] = useState ('A')
 
     useEffect(() => {
         const buscarGrupos = async () => {
@@ -14,41 +15,57 @@ function GroupStandings() {
         }
         buscarGrupos()
     }, [])
-    
+
     return(
-        <table className={styles.table}>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Seleção</th>
-                    <th>Pontos</th>
-                    <th>Vitórias</th>
-                    <th>Empates</th>
-                    <th>Derrotas</th>
-                    <th>Gols Pró</th>
-                    <th>Gols Contra</th>
-                    <th>Saldo de Gols</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    grupos.filter( grupo => grupo.grupo === "B" )
-                    .map( grupo => (
-                        <tr key={grupo.selecao}>
-                            <td>{grupo.posicao}</td>
-                            <td className={styles.esquerda}>{grupo.selecao}</td>
-                            <td>{grupo.pontos}</td>
-                            <td>{grupo.vitorias}</td>
-                            <td>{grupo.empates}</td>
-                            <td>{grupo.derrotas}</td>
-                            <td>{grupo.gols_pro}</td>
-                            <td>{grupo.gols_contra}</td>
-                            <td>{grupo.saldo_gols}</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+        <>
+            <div className={styles.div_select}>
+                <select 
+                    value={letraSelecionada} 
+                    onChange={(e) => setLetraSelecionada(e.target.value)}>
+                    <option value="A">Grupo A</option>
+                    <option value="B">Grupo B</option>
+                    <option value="C">Grupo C</option>
+                    <option value="D">Grupo D</option>
+                    <option value="E">Grupo E</option>
+                    <option value="F">Grupo F</option>
+                    <option value="G">Grupo G</option>
+                    <option value="H">Grupo H</option>
+                </select>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Seleção</th>
+                        <th>Pontos</th>
+                        <th>Vitórias</th>
+                        <th>Empates</th>
+                        <th>Derrotas</th>
+                        <th>Gols Pró</th>
+                        <th>Gols Contra</th>
+                        <th>Saldo de Gols</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        grupos.filter( grupo => grupo.grupo === letraSelecionada )
+                        .map( grupo => (
+                            <tr key={grupo.selecao}>
+                                <td>{grupo.posicao}</td>
+                                <td className={styles.esquerda}>{grupo.selecao}</td>
+                                <td>{grupo.pontos}</td>
+                                <td>{grupo.vitorias}</td>
+                                <td>{grupo.empates}</td>
+                                <td>{grupo.derrotas}</td>
+                                <td>{grupo.gols_pro}</td>
+                                <td>{grupo.gols_contra}</td>
+                                <td>{grupo.saldo_gols}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </>
     );
 }
 
